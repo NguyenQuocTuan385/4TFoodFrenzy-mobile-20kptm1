5f996d36336c1689f2481bfc70f96760ad6f561f
Thong <20127634@student.hcmus.edu.vn>
2023-04-02 17:25:42 +0700

    Hoàn thành layout xác thực địa chỉ email

---
+++ app/src/main/java/com/example/a4tfoodfrenzy/VerificationCodeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a4tfoodfrenzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerificationCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification_code)
+    }
+}
