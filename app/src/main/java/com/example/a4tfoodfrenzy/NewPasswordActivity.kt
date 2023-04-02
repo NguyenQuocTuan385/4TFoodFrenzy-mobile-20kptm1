eedaa670fd4d54958dcfa596b7e34bb79cf1c355
Thong <20127634@student.hcmus.edu.vn>
2023-04-02 18:08:15 +0700

    Hoàn thành layout đổi mật khẩu mới

---
+++ app/src/main/java/com/example/a4tfoodfrenzy/NewPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a4tfoodfrenzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+    }
+}
