50a2b43f4beb6d784624211beafab51482773ca9
Thong <20127634@student.hcmus.edu.vn>
2023-04-05 21:25:26 +0700

    Hoàn thành layout thêm nguyên liệu

---
+++ app/src/main/java/com/example/a4tfoodfrenzy/AddIngredient.kt
@@ -0,0 +1,11 @@
+package com.example.a4tfoodfrenzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddIngredient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_ingredient)
+    }
+}
