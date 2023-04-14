3cd77e1415bd77b10040f11a4d8bb8e76f1b0c42
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2023-04-14 19:02:07 +0700

    hoàn thành xem chi tiết bình luận ở admin

---
+++ app/src/main/java/com/example/a4tfoodfrenzy/CommentDetailsManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a4tfoodfrenzy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CommentDetailsManagementActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment_details_management)
+    }
+}
