29b2f3e7f9431eb7cfeb7bc500e0f784b69e26cf
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2023-04-14 19:03:43 +0700

    thêm link từ trang profile tới trang update profile và chỉnh sửa vài thứ

--- app/src/main/java/com/example/a4tfoodfrenzy/EditProfileActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/EditProfileActivity.kt
@@ -1,12 +1,21 @@
 package com.example.a4tfoodfrenzy
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.ActionMenuItemView
 
 class EditProfileActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
+        val btnUpdate = findViewById<ActionMenuItemView>(R.id.action_update)
+           btnUpdate.setOnClickListener {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
     }
 }
--- app/src/main/java/com/example/a4tfoodfrenzy/ProfileActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/ProfileActivity.kt
@@ -1,16 +1,47 @@
 package com.example.a4tfoodfrenzy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 
 class ProfileActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val option_adapter = findViewById<ImageView>(R.id.option_profile)
+        val list_option = listOf<String>("Chỉnh sửa thông tin", "Đăng xuất")
+        option_adapter.setOnClickListener {
+            val popup = PopupMenu(this, option_adapter)
+            for (i in list_option) {
+                popup.menu.add(i)
+            }
+            popup.setOnMenuItemClickListener { item ->
+                when (item.title) {
+                    "Chỉnh sửa thông tin" -> {
+                        val intent = Intent(this, EditProfileActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    "Đăng xuất" -> {
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popup.show()
+        }
+
+
         val adapter = TabAdapter(supportFragmentManager)
         adapter.addFragment(TabFragment1(), "Món đã lưu")
         adapter.addFragment(TabFragment2(), "Món của tôi")
