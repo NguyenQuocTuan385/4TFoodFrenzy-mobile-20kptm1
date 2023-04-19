c6ab07e3bfc0c0731e9d1b98061c5a7f2667254e
Thong <20127634@student.hcmus.edu.vn>
2023-04-19 11:17:58 +0700

    navigate các màn hình đăng ký, đăng nhập, quên mật khẩu

--- app/src/main/java/com/example/a4tfoodfrenzy/NewPasswordActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/NewPasswordActivity.kt
@@ -1,11 +1,31 @@
 package com.example.a4tfoodfrenzy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class NewPasswordActivity : AppCompatActivity() {
+    private lateinit var finishBtn: Button
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_password)
+
+        finishBtn=findViewById(R.id.finishBtn)
+        toolbar=findViewById(R.id.toolbar)
+
+        finishBtn.setOnClickListener {
+            val intent= Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+
     }
 }
