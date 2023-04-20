7780d8e85c9b99d1651e45796de162f82e8b5b80
Gia Tiến <truongtien577@gmail.com>
2023-04-21 02:24:39 +0700

    Thêm user mới vào firestore nếu đăng nhập facebook, google lần đầu tiên Kiểm tra việc đăng nhập không đúng provider (chỉ có facebook)

--- app/src/main/java/com/example/a4tfoodfrenzy/View/FacebookAuthenticateActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/View/FacebookAuthenticateActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a4tfoodfrenzy.Model.User
 import com.example.a4tfoodfrenzy.R
 import com.facebook.AccessToken
 import com.facebook.CallbackManager
@@ -13,14 +14,17 @@ import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FacebookAuthenticateActivity : AppCompatActivity() {
-    private lateinit var callbackManager : CallbackManager
+    private lateinit var callbackManager: CallbackManager
 
     private lateinit var auth: FirebaseAuth
-// ...
+
+    // ...
 // Initialize Firebase Auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +35,12 @@ class FacebookAuthenticateActivity : AppCompatActivity() {
         callbackManager = CallbackManager.Factory.create();
 
 
-        LoginManager.getInstance().logInWithReadPermissions(this, arrayListOf("email", "public_profile"))
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, arrayListOf("email", "public_profile"))
         LoginManager.getInstance().registerCallback(callbackManager,
             object : FacebookCallback<LoginResult> {
                 override fun onSuccess(loginResult: LoginResult) {
                     handleFacebookAccessToken(loginResult.accessToken)
-                    Toast.makeText(baseContext, "On success authentication",
-                        Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onCancel() {
@@ -58,32 +61,103 @@ class FacebookAuthenticateActivity : AppCompatActivity() {
     }
 
     private fun handleFacebookAccessToken(token: AccessToken) {
-
         val credential = FacebookAuthProvider.getCredential(token.token)
+
+
+
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     val user = auth.currentUser
-                    Toast.makeText(baseContext, "Authentication success. ${user?.displayName}",
-                        Toast.LENGTH_SHORT).show()
+
+                    // get current user's email
+                    val userEmail = user?.email
+                    val db = Firebase.firestore
+
+                    db.collection("users")
+                        .whereEqualTo("email", userEmail)
+                        .get()
+                        .addOnSuccessListener {
+                            // user not exist in DB --> add user
+                            if (it.isEmpty) {
+                                // get user name from google's database
+                                val userFullName = user?.displayName
+                                val profile = User(
+                                    1125,
+                                    userEmail!!,
+                                    "123456",
+                                    userFullName!!,
+                                    null,
+                                    "",
+                                    R.drawable.defaultavt,
+                                    arrayListOf(),
+                                    arrayListOf(),
+                                    arrayListOf()
+                                )
+
+                                // add new user to db
+                                db.collection("users").document(user.uid)
+                                    .set(profile)
+                                    .addOnSuccessListener {
+                                        Toast.makeText(
+                                            this,
+                                            "Đăng ký bằng Facebook thành công",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(
+                                            this,
+                                            "Đăng ký bằng Facebook không thành công",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                            }
+                            // user exist in DB --> do nothing
+                        }
+                        .addOnFailureListener { exception ->
+                        }
+
+                    // display success message
+                    Toast.makeText(
+                        baseContext, "Đăng nhập bằng Facebook thành công",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // success --> to home
                     val toHomeIntent = Intent(this, MainActivity::class.java)
+
+                    // remove all previous intent (activity)
+                    toHomeIntent.flags =
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(toHomeIntent)
-                    this@FacebookAuthenticateActivity.finish()
-//                    updateUI(user)
+
                 } else {
                     // If sign in fails, display a message to the user.
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Đăng nhập bằng Facebook thất bại",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // email registered with a different provider
+                    if (task.exception is FirebaseAuthUserCollisionException)
+                        Toast.makeText(
+                            baseContext,
+                            "Vui lòng đăng nhập bằng Google",
+                            Toast.LENGTH_LONG
+                        ).show()
 
                     // failed --> back to sign in
                     val toLoginPage = Intent(this, LoginRegisterActivity::class.java)
+
+                    // remove all previous intent (activity)
+                    toLoginPage.flags =
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(toLoginPage)
-                    this@FacebookAuthenticateActivity.finish()
-//                    updateUI(null)
                 }
             }
     }
 
-    private fun isSignUp(){}
+    private fun isSignUp() {}
 }
--- app/src/main/java/com/example/a4tfoodfrenzy/View/GoogleAuthenticateActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/View/GoogleAuthenticateActivity.kt
@@ -2,8 +2,10 @@ package com.example.a4tfoodfrenzy.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a4tfoodfrenzy.Model.User
 import com.example.a4tfoodfrenzy.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -11,10 +13,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class GoogleAuthenticateActivity : AppCompatActivity() {
@@ -30,7 +31,8 @@ class GoogleAuthenticateActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this@GoogleAuthenticateActivity, googleSignInOptions)
+        googleSignInClient =
+            GoogleSignIn.getClient(this@GoogleAuthenticateActivity, googleSignInOptions)
 
         val intent: Intent = googleSignInClient.signInIntent
         startActivityForResult(intent, 100)
@@ -46,9 +48,7 @@ class GoogleAuthenticateActivity : AppCompatActivity() {
             val signInAccountTask: Task<GoogleSignInAccount> =
                 GoogleSignIn.getSignedInAccountFromIntent(data)
 
-            val acc : GoogleSignInAccount = signInAccountTask.result
-
-            Toast.makeText(this,acc.displayName, Toast.LENGTH_LONG).show()
+            val acc: GoogleSignInAccount = signInAccountTask.result
 
             // check condition
             if (signInAccountTask.isSuccessful) {
@@ -62,26 +62,99 @@ class GoogleAuthenticateActivity : AppCompatActivity() {
                         val authCredential: AuthCredential = GoogleAuthProvider.getCredential(
                             googleSignInAccount.idToken, null
                         )
+
                         // Check credential
                         firebaseAuth.signInWithCredential(authCredential)
                             .addOnCompleteListener(this) { task ->
                                 // Check condition
                                 if (task.isSuccessful) {
-                                    // When task is successful redirect to profile activity
+                                    // get current user's email
+                                    val userEmail = firebaseAuth.currentUser?.email
+                                    val db = Firebase.firestore
 
+                                    // query to check if email of current user exist
+                                    db.collection("users")
+                                        .whereEqualTo("email", userEmail)
+                                        .get()
+                                        .addOnSuccessListener {
+                                            // user not exist in DB --> add user
+                                            if (it.isEmpty) {
+                                                // get user name from google's database
+                                                val userFullName =
+                                                    firebaseAuth.currentUser?.displayName
+                                                val profile = User(
+                                                    1125, userEmail!!, "123456",
+                                                    userFullName!!,
+                                                    null,
+                                                    "",
+                                                    R.drawable.defaultavt,
+                                                    arrayListOf(),
+                                                    arrayListOf(),
+                                                    arrayListOf()
+                                                )
+
+                                                // add new user to db
+                                                db.collection("users")
+                                                    .document(firebaseAuth.currentUser!!.uid)
+                                                    .set(profile)
+                                                    .addOnSuccessListener {
+                                                        Toast.makeText(
+                                                            this,
+                                                            "Đăng ký bằng Google thành công",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    }
+                                                    .addOnFailureListener {
+                                                        Toast.makeText(
+                                                            this,
+                                                            "Đăng ký bằng Google không thành công",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    }
+                                            }
+                                            // user exist in DB --> do nothing
+                                        }
+                                        .addOnFailureListener {
+                                        }
+
+                                    // When task is successful redirect to profile activity
                                     // Display Toast
-                                    Toast.makeText(this, "Firebase authentication successful", Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(
+                                        this,
+                                        "Đăng nhập bằng Google thành công",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    // success --> load home
                                     val toHomeIntent = Intent(this, MainActivity::class.java)
+
+                                    // remove all previous intent (activity)
+                                    toHomeIntent.flags =
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                                     startActivity(toHomeIntent)
-                                    this@GoogleAuthenticateActivity.finish()
                                 } else {
-                                    // When task is unsuccessful display Toast
-                                    Toast.makeText(this, "Authentication Failed : ${task.exception?.message}" , Toast.LENGTH_SHORT).show()
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(
+                                        baseContext, "Đăng nhập bằng Google thất bại",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    // email registered with a different provider
+                                    if (task.exception is FirebaseAuthUserCollisionException)
+                                        Toast.makeText(
+                                            baseContext,
+                                            "Tài khoản email đã được đăng ký bằng một dịch vụ khác, vui lòng truy cập đúng dịch vụ mà bạn đã đăng ký",
+                                            Toast.LENGTH_LONG
+                                        ).show()
 
                                     // failed --> back to sign in
-                                    val toLoginPage = Intent(this, LoginRegisterActivity::class.java)
+                                    val toLoginPage =
+                                        Intent(this, LoginRegisterActivity::class.java)
+
+                                    // remove all previous intent (activity)
+                                    toLoginPage.flags =
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                                     startActivity(toLoginPage)
-                                    this@GoogleAuthenticateActivity.finish()
                                 }
                             }
                     }
