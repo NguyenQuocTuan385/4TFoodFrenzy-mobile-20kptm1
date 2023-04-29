6218a0dc846dbe21bdd8de1c099a53e6bc2d8b0f
Thong <20127634@student.hcmus.edu.vn>
2023-04-29 23:11:10 +0700

    thêm popmenu và naviagte đến màn hình cập nhật thông tin người dùng

--- app/src/main/java/com/example/a4tfoodfrenzy/View/Fragment/AdminProfileManagement.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/View/Fragment/AdminProfileManagement.kt
@@ -1,20 +1,21 @@
 package com.example.a4tfoodfrenzy.View.Fragment
 
+import android.content.Intent
 import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.SearchView
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a4tfoodfrenzy.Adapter.UserAdapter
+import com.example.a4tfoodfrenzy.Helper.HelperFunctionDB
 import com.example.a4tfoodfrenzy.Model.DBManagement
 import com.example.a4tfoodfrenzy.Model.User
 import com.example.a4tfoodfrenzy.R
+import com.example.a4tfoodfrenzy.View.AccountDetailsManagementActivity
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -34,6 +35,8 @@ class AdminProfileManagement : Fragment() {
         listUser.addAll(DBManagement.userList)
         filerUsers()
         userAdapter= UserAdapter(requireContext(),listUser)
+        optionMenu()
+        setOnClickItem()
         val recyclerView= view?.findViewById<RecyclerView>(R.id.list_user)
         recyclerView?.adapter=userAdapter
         recyclerView?.layoutManager=GridLayoutManager(requireContext(),2, GridLayoutManager.VERTICAL,false)
@@ -62,6 +65,48 @@ class AdminProfileManagement : Fragment() {
             }
         })
 
+    }
+
+    private fun optionMenu() {
+        userAdapter.onButtonClick = { view, user ->
+            val popUpMenu = PopupMenu(requireContext(), view)
+            popUpMenu.menuInflater.inflate(R.menu.menu_ingredient, popUpMenu.menu)
+            popUpMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.update-> {
+                        sendData(user)
+                        true
+                    }
+                    R.id.delete-> {
+                        var helperFunctionDB= HelperFunctionDB(requireContext())
+                        helperFunctionDB.showWarningAlert("Xóa người dùng",
+                            "Bạn có chắc là sẽ xóa người dùng này?")
+                        {confirm ->
+                            if(confirm)
+                            {
+
+                            }
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popUpMenu.show()
+        }
+
+    }
+    private fun setOnClickItem()
+    {
+        userAdapter.onItemClick={user ->
+            sendData(user)
+        }
+    }
+    private fun sendData(user:User)
+    {
+        val intent= Intent(requireContext(),AccountDetailsManagementActivity::class.java)
+        intent.putExtra("user",user)
+        startActivity(intent)
     }
 
 
