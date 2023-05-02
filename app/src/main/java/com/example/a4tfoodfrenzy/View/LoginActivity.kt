c6e2cd25a02f0ff891ba2d2c67aa86ab9169157f
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2023-05-02 12:41:32 +0700

    Fix lỗi sau khi đăng ký và đăng nhập thì bị các lỗi về tương tác chức năng

--- app/src/main/java/com/example/a4tfoodfrenzy/View/LoginActivity.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/View/LoginActivity.kt
@@ -75,6 +75,7 @@ class LoginActivity : AppCompatActivity() {
                                 finish()
                             } else {
                                 val intent= Intent(this, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                 startActivity(intent)
                                 finish()
                             }
