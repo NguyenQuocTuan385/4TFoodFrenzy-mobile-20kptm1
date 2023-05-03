35cd9c4dd00f27ddf22ac2b8a8498ae9621cf532
Gia Tiến <truongtien577@gmail.com>
2023-05-03 17:58:00 +0700

    Gán data cho quản lý công thức (admin) Phân trang, hiển thị item cho chức năng quản lý công thức Chỉnh lại layout cho quản lý công thức fix null user khi trở về trang trước ở viết bình luận

--- app/src/main/java/com/example/a4tfoodfrenzy/Adapter/RecipeManagementAdapter.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/Adapter/RecipeManagementAdapter.kt
@@ -1,25 +1,35 @@
 package com.example.a4tfoodfrenzy.Adapter
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.a4tfoodfrenzy.Model.DBManagement
 import com.example.a4tfoodfrenzy.Model.FoodRecipe
 import com.example.a4tfoodfrenzy.R
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import de.hdodenhof.circleimageview.CircleImageView
 
-class RecipeManagementAdapter(private var context:Context,private var recipeList : ArrayList<FoodRecipe>) : RecyclerView.Adapter<RecipeManagementAdapter.ViewHolder>(){
-    inner class ViewHolder(listItemView : View) : RecyclerView.ViewHolder(listItemView){
-        val recipeIMG : ImageView = listItemView.findViewById(R.id.recipeManagementImageView)
-        val recipeName : TextView = listItemView.findViewById(R.id.recipeNameManagementTextView)
-        val numOfLike : TextView = listItemView.findViewById(R.id.numOfLikesTextView)
-        val authorName : TextView = listItemView.findViewById(R.id.authorName)
-        val authorAvatarIMG : CircleImageView = listItemView.findViewById(R.id.avatarImageView)
-        val uploadDate : TextView = listItemView.findViewById(R.id.uploadDateTextView)
+class RecipeManagementAdapter(
+    private var context: Context,
+    private var recipeList: ArrayList<FoodRecipe>
+) : RecyclerView.Adapter<RecipeManagementAdapter.ViewHolder>() {
+    val storageRef = Firebase.storage
+    var start = 0
+    var end = 5
+
+    inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
+        val recipeIMG: ImageView = listItemView.findViewById(R.id.recipeManagementImageView)
+        val recipeName: TextView = listItemView.findViewById(R.id.recipeNameManagementTextView)
+        val numOfLike: TextView = listItemView.findViewById(R.id.numOfLikesTextView)
+        val authorName: TextView = listItemView.findViewById(R.id.authorName)
+        val authorAvatarIMG: CircleImageView = listItemView.findViewById(R.id.avatarImageView)
+        val uploadDate: TextView = listItemView.findViewById(R.id.uploadDateTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,17 +44,52 @@ class RecipeManagementAdapter(private var context:Context,private var recipeList
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentRecipe = recipeList[position]
-        val uploadDateString = "${currentRecipe.date?.date}/${currentRecipe.date?.month}/${currentRecipe.date?.year}"
+        // not in pagination item range --> hide
+        if (position < start || position > end) {
+            holder.itemView.visibility = View.GONE
+            holder.itemView.layoutParams = RecyclerView.LayoutParams(0, 0)
+            return
+        }
 
-        val resources = context.getResources()
-        val resourceId = resources.getIdentifier(currentRecipe.recipeMainImage, "drawable", context.packageName)
-        val bitmap = BitmapFactory.decodeResource(resources, resourceId)
-        holder.recipeIMG.setImageBitmap(bitmap)
+        // visible when in pagination range
+        holder.itemView.visibility = View.VISIBLE
+        holder.itemView.layoutParams =
+            RecyclerView.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+        val currentRecipe = recipeList[position]
+        val uploadDateString =
+            "${currentRecipe.date.date}/${currentRecipe.date.month}/${currentRecipe.date.year}"
+
+        // assign main food image
+        val foodImageRef = currentRecipe.recipeMainImage?.let { storageRef.getReference(it) }
+
+        foodImageRef?.downloadUrl?.addOnSuccessListener { uri ->
+            Glide.with(context)
+                .load(uri)
+                .into(holder.recipeIMG)
+        }?.addOnFailureListener {}
+
+        // find author
+        for (user in DBManagement.userList) {
+            if (user.myFoodRecipes.contains(currentRecipe.id)) {
+                val authorAvtRef = user.avatar.let { storageRef.getReference(it) }
+
+                // assign author avatar
+                authorAvtRef.downloadUrl.addOnSuccessListener { uri ->
+                    Glide.with(context)
+                        .load(uri)
+                        .into(holder.authorAvatarIMG)
+                }.addOnFailureListener {}
+                break
+            }
+        }
 
         holder.recipeName.text = currentRecipe.recipeName
-        holder.numOfLike.text = currentRecipe.numOfLikes.toString()
-        currentRecipe.authorAvatar?.let { holder.authorAvatarIMG.setImageResource(it) }
+        holder.numOfLike.text =
+            if (currentRecipe.numOfLikes != null) currentRecipe.numOfLikes.toString() else "0"
         holder.authorName.text = currentRecipe.authorName
         holder.uploadDate.text = uploadDateString
     }
--- app/src/main/java/com/example/a4tfoodfrenzy/View/Fragment/AdminRecipeManagement.kt
+++ app/src/main/java/com/example/a4tfoodfrenzy/View/Fragment/AdminRecipeManagement.kt
@@ -1,20 +1,21 @@
 package com.example.a4tfoodfrenzy.View.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.PopupMenu
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a4tfoodfrenzy.Adapter.RecipeManagementAdapter
-import com.example.a4tfoodfrenzy.Model.FoodRecipe
+import com.example.a4tfoodfrenzy.Model.DBManagement
 import com.example.a4tfoodfrenzy.R
-import java.util.*
-import kotlin.collections.ArrayList
+
 
 class AdminRecipeManagement : Fragment() {
 
@@ -30,54 +31,48 @@ class AdminRecipeManagement : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_admin_recipe_management, container, false)
 
-        val recipeManagementRecyclerView : RecyclerView = view.findViewById(R.id.adminRecipeManagementRecyclerView)
-        val optionAdapter : ImageView = view.findViewById(R.id.recipeManagementFilterImageView)
-        val filterOptionTV : TextView = view.findViewById(R.id.recipeManagementFilterTextView)
+        // layout view
+        val recipeManagementRecyclerView: RecyclerView =
+            view.findViewById(R.id.adminRecipeManagementRecyclerView)
+        val optionAdapter: ImageView = view.findViewById(R.id.recipeManagementFilterImageView)
+        val filterOptionTV: TextView = view.findViewById(R.id.recipeManagementFilterTextView)
+        val loadMoreButton: Button = view.findViewById(R.id.loadMoreRecipeButton)
 
-        val recipeList = arrayListOf<FoodRecipe>()
-        var adapter : RecipeManagementAdapter? = null
+        val recipeList = DBManagement.foodRecipeList
+        var adapter: RecipeManagementAdapter? = null
         val optionList = arrayListOf("Mới nhất", "Phổ biến", "Nhiều lượt thích nhất")
 
-        recipeList.add(
-            FoodRecipe(1, "Bò xốt me",
-            "bo_nuong", 2, "15 phút", Date(2022, 2,2), true,
-            ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), "Đặng Ngọc Tiến",
-            R.drawable.avt, 100)
-        )
-        recipeList.add(
-            FoodRecipe(1, "Bò xốt me",
-            "bo_nuong", 2, "15 phút", Date(2022, 2,2), true,
-            ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), "Đặng Ngọc Tiến",
-            R.drawable.avt, 100)
-        )
-        recipeList.add(
-            FoodRecipe(1, "Bò xốt me",
-            "bo_nuong", 2, "15 phút", Date(2022, 2,2), true,
-            ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), "Đặng Ngọc Tiến",
-            R.drawable.avt, 100)
-        )
-        recipeList.add(
-            FoodRecipe(1, "Bò xốt me",
-            "bo_nuong", 2, "15 phút", Date(2022, 2,2), true,
-            ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), "Đặng Ngọc Tiến",
-            R.drawable.avt, 100)
-        )
-
         // assign recipe recycler view adapter
-        adapter = RecipeManagementAdapter(requireContext(),recipeList)
+        adapter = RecipeManagementAdapter(requireContext(), recipeList)
         recipeManagementRecyclerView.adapter = adapter
         recipeManagementRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
 
+        // remove item change animation
+        recipeManagementRecyclerView.itemAnimator = null
+
+        // view more food
+        loadMoreButton.setOnClickListener {
+            adapter.start += 6
+            adapter.end += 6
+
+            if (adapter.end >= recipeList.size) {
+                adapter.start = 0
+                adapter.end = 5
+            }
+
+            adapter.notifyItemRangeChanged(0, adapter.end + 1)
+        }
+
         // assign recipe filter pop-up
-        optionAdapter.setOnClickListener{
+        optionAdapter.setOnClickListener {
             val popUp = PopupMenu(requireContext(), optionAdapter)
 
             // add option string to pop-up
-            for(option in optionList)
+            for (option in optionList)
                 popUp.menu.add(option)
 
-            popUp.setOnMenuItemClickListener{ item ->
-                when (item.title){
+            popUp.setOnMenuItemClickListener { item ->
+                when (item.title) {
                     "Mới nhất" -> {
                         filterOptionTV.text = item.title
                         true
@@ -96,6 +91,7 @@ class AdminRecipeManagement : Fragment() {
 
             popUp.show()
         }
+
         return view
     }
 
